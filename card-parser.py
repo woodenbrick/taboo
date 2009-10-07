632caf773a6642c3c4096bd4fa5ae531ce72720e
Daniel Woodhouse <wodemoneke@gmail.com>
2009-10-07 20:53:47 +0100

    updated fonts

--- card-parser.py
+++ card-parser.py
@@ -16,19 +16,32 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS `cards` (
                 `keyword4` varchar(50),
                 `keyword5` varchar(50)
             )""")
+cursor.execute("""CREATE TABLE IF NOT exists `stupidcards` (
+                `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+                `word` varchar(50),
+                `keyword1` varchar(50),
+                `keyword2` varchar(50)
+            )""")
 conn.commit()
-total = 500
+total = 5
 count = 1
 while count <= total:    
     data = urllib.urlopen("http://www.eseonline.net/taboo/taboo.php").read()
     word = re.search("<FONT COLOR=RED>([a-zA-Z ]+)", data)
     if word is None:
         continue
-    indb = cursor.execute("""SELECT word FROM cards WHERE word=?""", (word.group(1),)).fetchone()
+    keywords = string.split(re.search("<font color=#666666>(.+)</font>", data).group(1), "<BR />")
+    indb = cursor.execute("""SELECT word FROM cards WHERE word=? and keyword1=? and keyword2=?""",
+                          (word.group(1), keywords[0].title(), keywords[1].title())).fetchone()
     if indb:
         print 'card exists, discarding'
         continue
-    keywords = string.split(re.search("<font color=#666666>(.+)</font>", data).group(1), "<BR />")
+    stupid = cursor.execute("""SELECT word FROM stupidcards WHERE word=? and keyword1=? and
+                            keyword2=?""", (word.group(1), keywords[0].title(), keywords[1].title())).fetchone()
+    if stupid:
+        print 'card has been marked as stupid, discarding'
+        continue
+    
     cursor.execute("""INSERT INTO cards (word, keyword1, keyword2, keyword3, keyword4,
                    keyword5) VALUES (?, ?, ?, ?, ?, ?)""", (word.group(1).title(), keywords[0].title(),
                                                             keywords[1].title(), keywords[2].title(),
--- taboo.py
+++ taboo.py
@@ -27,10 +27,10 @@ class Game():
             return
         if entrybox.name == "team_a_entry":
             self.team_a.append(entrybox.get_text())
-            self.wTree.get_widget("team_a_list").set_text("\n".join(self.team_a))
+            self.wTree.get_widget("team_a_list").set_markup('<span size="20000">' + "\n".join(self.team_a) + "</span>")
         else:
             self.team_b.append(entrybox.get_text())
-            self.wTree.get_widget("team_b_list").set_text("\n".join(self.team_b))
+            self.wTree.get_widget("team_b_list").set_markup('<span size="20000">' + "\n".join(self.team_b) + "</span>")
         entrybox.set_text("")
         self.wTree.get_widget("setup_error").set_text("")
         
@@ -48,27 +48,38 @@ class Game():
         self.new_round = True
         self.round = 0
         self.time_passed = 0
-        self.wTree.get_widget("whose_turn").set_text("""It's %s's turn.""" % self.team_a[self.team_a_turn])
+        self.ten_second_counter = 0
+        self.wTree.get_widget("whose_turn").set_markup("""It's <big>%s's</big> turn.""" % self.team_a[self.team_a_turn])
         self.wTree.get_widget("wait_screen").show()
 
 
     def on_start_clicked(self, button):
         self.wTree.get_widget("wait_screen").hide()
         self.temp_score = 0
+        self.update_timer()
         gobject.timeout_add(1000, self.update_timer)
         self.show_card()
         self.wTree.get_widget("main_screen").show()
         
     def show_card(self):
-        result = self.cursor.execute("""SELECT word, keyword1, keyword2, keyword3, keyword4,
+        self.result = self.cursor.execute("""SELECT word, keyword1, keyword2, keyword3, keyword4,
                                       keyword5 FROM cards WHERE id=?""", (self.cards.pop(),)).fetchone()
-        self.wTree.get_widget("card").set_text(result[0] + "\n\n" + "\n".join(result[1:]))
+        self.wTree.get_widget("card").set_markup("<span size='20000'><u>" + self.result[0] +
+                                                 "</u></span>\n\n<span size='15000'>" +
+                                                 "\n".join(self.result[1:]) + "</span>")
     
     def next_card(self, button):
+        self.ten_second_counter = 0
         if button.name == "success":
             self.temp_score += 1
         elif button.name == "pass":
             self.temp_score -= 1
+        else:
+            self.cursor.execute("""DELETE FROM cards where word=? and keyword1=? and keyword2=?""",
+                                (self.result[0], self.result[1], self.result[2]))
+            self.cursor.execute("""INSERT INTO stupidcards (word, keyword1, keyword2) VALUES
+                                (?, ?, ?)""", (self.result[0], self.result[1], self.result[2]))
+            self.conn.commit()
         if self.new_round:
             self.wTree.get_widget("team_a_score2").set_text(str(self.temp_score + self.team_a_score))
         else:
@@ -77,6 +88,7 @@ class Game():
         
     def update_timer(self):
         time_since = self.length_of_rounds * 60 - self.time_passed
+        self.ten_second_counter += 1
         if time_since == 0:
             self.end_round()
             return False
@@ -88,11 +100,22 @@ class Game():
                 time_split[i] = str("0" + str(time_split[i]))
             else:
                 time_split[i] = str(time_split[i])
-        self.wTree.get_widget("timer").set_text(":".join(time_split))
+        self.wTree.get_widget("timer").set_markup("<span size='40000'>" + ":".join(time_split) + "</span>")
         self.time_passed += 1
         return True
     
     def end_round(self):
+        penalty = "No penalty as you had less than 10 seconds for this card." if self.ten_second_counter < 11 else ""
+        popup = gtk.MessageDialog(None, gtk.DIALOG_MODAL,
+        gtk.MESSAGE_INFO, gtk.BUTTONS_YES_NO,
+        """Did you get the last card correct? (Word was %s. %s)\n""" % (self.result[0], penalty))
+        response = popup.run()
+        if response == gtk.RESPONSE_YES:
+            self.temp_score += 1
+        else:
+            if self.ten_second_counter > 10:
+                self.temp_score -= 1
+        popup.destroy()
         self.new_round = not self.new_round
         if self.new_round:
             self.round += 1
@@ -101,19 +124,18 @@ class Game():
             self.team_b_turn += 1
             if self.team_b_turn == len(self.team_b):
                 self.team_b_turn = 0
-            self.wTree.get_widget("whose_turn").set_text("""%s\nIt's %s's turn.""" % (score_str,self.team_a[self.team_a_turn]))
+            self.wTree.get_widget("whose_turn").set_markup("""%s\nIt's <big>%s's</big> turn.""" % (score_str,self.team_a[self.team_a_turn]))
         else:
             self.team_a_score += self.temp_score
             score_str = self.score_str(self.team_a[self.team_a_turn])
             self.team_a_turn += 1
             if self.team_a_turn == len(self.team_a):
                 self.team_a_turn = 0
-            self.wTree.get_widget("whose_turn").set_text("""%s\nIt's %s's turn.""" % (score_str, self.team_b[self.team_b_turn]))
+            self.wTree.get_widget("whose_turn").set_markup("""%s\nIt's <big>%s's</big> turn.""" % (score_str, self.team_b[self.team_b_turn]))
+        self.wTree.get_widget("team_a_score").set_text(str(self.team_a_score))
+        self.wTree.get_widget("team_b_score").set_text(str(self.team_b_score))
         if self.num_of_rounds != self.round:
-            self.time_passed = 0
-            self.wTree.get_widget("team_a_score").set_text(str(self.team_a_score))
-            self.wTree.get_widget("team_b_score").set_text(str(self.team_b_score))
-            
+            self.time_passed = 0    
         else:
             #game over
             if self.team_a_score == self.team_b_score:
@@ -123,7 +145,7 @@ class Game():
                     winners = self.team_a
                 else:
                     winners = self.team_b
-                self.wTree.get_widget("whose_turn").set_text("Game Over!\n %s win!" % "\n".join(winners))
+                self.wTree.get_widget("whose_turn").set_markup("Game Over!\n The winners are:<big>%s</big>" % "\n".join(winners))
             self.wTree.get_widget("go").hide()
             self.wTree.get_widget("wait_quit").show()
             self.wTree.get_widget("new_game").show()
@@ -132,7 +154,7 @@ class Game():
 
     def score_str(self, player):
         descriptors = ["dismal", "poor", "lackluster", "decent", "fantastic", "incredible"]
-        return "%s scored %s %s this round" % (player, self.temp_score,
+        return "<span size='20000'>%s</span> scored %s %s this round" % (player, self.temp_score,
                                                "point" if self.temp_score == 1 else "points")
             
 
@@ -149,7 +171,8 @@ class Game():
         self.wTree.get_widget("team_b_list").set_text("")
         self.wTree.get_widget("team_a_list").set_text("")
         
-    def gtk_quit(self, widget):
+    def gtk_main_quit(self, widget):
+        self.conn.close()
         gtk.main_quit()
     
 Game()
