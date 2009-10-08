7e95d9b201b27b26d3fc6a7c88f4e199e11ba350
Daniel Woodhouse <wodemoneke@gmail.com>
2009-10-08 12:31:01 +0100

    can download words with newline chars

--- card-parser.py
+++ card-parser.py
@@ -23,29 +23,32 @@ cursor.execute("""CREATE TABLE IF NOT exists `stupidcards` (
                 `keyword2` varchar(50)
             )""")
 conn.commit()
-total = 5
+total = 500
 count = 1
 while count <= total:    
     data = urllib.urlopen("http://www.eseonline.net/taboo/taboo.php").read()
-    word = re.search("<FONT COLOR=RED>([a-zA-Z ]+)", data)
+    word = re.search("<FONT COLOR=RED>([a-zA-Z \n]+)</FONT>", data)
     if word is None:
         continue
+    word = word.group(1).replace("\n", " ").title()
     keywords = string.split(re.search("<font color=#666666>(.+)</font>", data).group(1), "<BR />")
+    for i, key in enumerate(keywords):
+        keywords[i] = keywords[i].title().strip()
     indb = cursor.execute("""SELECT word FROM cards WHERE word=? and keyword1=? and keyword2=?""",
-                          (word.group(1), keywords[0].title(), keywords[1].title())).fetchone()
+                          (word, keywords[0], keywords[1])).fetchone()
     if indb:
         print 'card exists, discarding'
         continue
     stupid = cursor.execute("""SELECT word FROM stupidcards WHERE word=? and keyword1=? and
-                            keyword2=?""", (word.group(1), keywords[0].title(), keywords[1].title())).fetchone()
+                            keyword2=?""", (word, keywords[0], keywords[1])).fetchone()
     if stupid:
         print 'card has been marked as stupid, discarding'
         continue
     
     cursor.execute("""INSERT INTO cards (word, keyword1, keyword2, keyword3, keyword4,
-                   keyword5) VALUES (?, ?, ?, ?, ?, ?)""", (word.group(1).title(), keywords[0].title(),
-                                                            keywords[1].title(), keywords[2].title(),
-                                                            keywords[3].title(), keywords[4].title()))
+                   keyword5) VALUES (?, ?, ?, ?, ?, ?)""", (word, keywords[0],
+                                                            keywords[1], keywords[2],
+                                                            keywords[3], keywords[4]))
     conn.commit()
     print "Card %d of %d" % (count, total)
     time.sleep(2)
